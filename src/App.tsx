3c12f3c9d38a18804054dabe2e719b53ff8e9595
SKAT-16 <nathangetachew536@gmail.com>
2025-02-21 08:06:00 +0300

    Finish chatbot UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ChatbotIcon from "./components/ChatbotIcon";
+import ChatForm from "./components/ChatForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="bg-white w-[420px] relative rounded-[15px] shadow-md overflow-hidden">
+        {/* Chat Header */}
+        <header className="bg-[#6d4fc2] fill-[#6d4fc2] shrink-0 flex items-center justify-between px-4 py-3">
+          <div className="flex gap-3 items-center">
+            <ChatbotIcon className="h-[35px] w-[35px] p-1.5 bg-white rounded-[50%]" />
+            <h2 className="text-white text-2xl font-semibold">Chatbot</h2>
+          </div>
+          <button className="material-symbols-rounded h-10 w-10 cursor-pointer text-white hover:bg-[#593bab] rounded-[50%] transition-all duration-200 ease-in-out">
+            keyboard_arrow_down
+          </button>
+        </header>
+
+        {/* Chat Body */}
+        <div className="h-[460px] mb-20 overflow-auto px-6 py-5 flex flex-col gap-5">
+          <div className="flex gap-3 items-center">
+            <ChatbotIcon className="h-[35px] w-[35px] p-1.5 fill-white bg-[#6D4FC2] mb-0.5 self-end rounded-[50%]" />
+            <p className="px-3 py-4 max-w-[75%] bg-[#F6F2FF] rounded-lg break-words whitespace-pre-line">
+              Hey there 👋
+              <br /> How can i help you today?
+            </p>
+          </div>
+
+          <div className="flex-col flex items-end">
+            <p className="px-3 py-4 max-w-[75%] bg-[#6D4FC2] text-white rounded-2xl rounded-br-sm">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem
+              sint eligendi voluptate nisi consequatur!
+            </p>
+          </div>
+        </div>
+
+        {/* Chat Footer */}
+        <div className="absolute bottom-0 left-0 px-5 pt-4 pb-5 bg-white w-full">
+          <ChatForm />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
